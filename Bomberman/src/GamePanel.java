f014e31a0ea992f24c50480742310af2cab6c620
Thuong Nguyen <nhthuong0826@gmail.com>
2025-03-21 23:49:37 +0700

    Thuong

---
+++ Bomberman/src/GamePanel.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements Runnable {
+
+    //SCREEN SETTINGS
+    final int originalTileSize = 16; // 16 pixels * 16 pixels
+    final int scale = 3;
+
+    final int tileSize = originalTileSize * scale; // 48 * 48
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol; // 768
+    final int screenHeight = tileSize * maxScreenRow; // 576
+
+    Thread gameThread;
+
+    public GamePanel() {
+
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+
+
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
