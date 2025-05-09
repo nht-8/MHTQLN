98c4f5a73d9ec66ec3b561a3055bfabdb6a09762
Ruby040207 <24022419@vnu.edu.vn>
2025-05-10 03:41:20 +0700

    Update Wall.java

--- bomberman/src/main/java/src/bomberman/entities/Wall.java
+++ bomberman/src/main/java/src/bomberman/entities/Wall.java
@@ -1,18 +1,14 @@
-// src/main/java/src/bomberman/entities/Wall.java
-package src.bomberman.entities; // HOẶC uet.oop.bomberman.entities
+package src.bomberman.entities; 
 
 import src.bomberman.graphics.Sprite;
 import src.bomberman.graphics.SpriteSheet;
 import java.util.List;
 
-/**
- * Đại diện cho một bức tường không thể phá hủy.
- */
 public class Wall extends Entity {
 
     public Wall(double xTile, double yTile, SpriteSheet sheet) {
         super(xTile, yTile, sheet);
-        this.sprite = Sprite.wall; // Gán sprite tường
+        this.sprite = Sprite.wall; 
         if (this.sprite == null) {
             System.err.println("CRITICAL WARNING: Sprite.wall is null during Wall construction!");
         }
@@ -20,12 +16,9 @@ public class Wall extends Entity {
 
     @Override
     public void update(double deltaTime, List<Entity> entities) {
-        // Tường không có logic cập nhật động
+
     }
 
-    /**
-     * Tường luôn là vật cản rắn.
-     */
     @Override
     public boolean isSolid() {
         return true;
