d1b45477a40a403bb15dfff26d3e58c44619eb23
Ruby040207 <24022419@vnu.edu.vn>
2025-05-10 03:22:31 +0700

    Update module-info.java

--- bomberman/src/main/java/module-info.java
+++ bomberman/src/main/java/module-info.java
@@ -5,8 +5,8 @@ module bomberman {
     requires javafx.fxml;
     requires java.desktop;
 
-    opens src.bomberman to javafx.graphics, javafx.fxml; // << THÊM javafx.fxml VÀO ĐÂY
-    opens src.bomberman.graphics to javafx.graphics; // Nếu có Node trong package này
-    opens src.bomberman.entities to javafx.graphics; // Nếu có Node trong package này
+    opens src.bomberman to javafx.graphics, javafx.fxml;
+    opens src.bomberman.graphics to javafx.graphics; 
+    opens src.bomberman.entities to javafx.graphics;
 
 }
