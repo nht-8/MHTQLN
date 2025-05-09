389d53ab55354e2eb7cf7736037b42814fd4a3f8
Ruby040207 <24022419@vnu.edu.vn>
2025-05-10 03:21:19 +0700

    Update module-info.java

--- bomberman/src/main/java/module-info.java
+++ bomberman/src/main/java/module-info.java
@@ -1,13 +1,10 @@
-// src/main/java/module-info.java
-module bomberman { // Đặt tên module của bạn (ví dụ: bomberman hoặc uet.oop.bomberman)
+module bomberman { 
     requires javafx.controls;
     requires javafx.media;
     requires javafx.graphics;
     requires javafx.fxml;
     requires java.desktop;
 
-    // Mở các package chứa Node và tài nguyên cho JavaFX
-    // Sửa lại tên package cho đúng với project của bạn
     opens src.bomberman to javafx.graphics, javafx.fxml; // << THÊM javafx.fxml VÀO ĐÂY
     opens src.bomberman.graphics to javafx.graphics; // Nếu có Node trong package này
     opens src.bomberman.entities to javafx.graphics; // Nếu có Node trong package này
