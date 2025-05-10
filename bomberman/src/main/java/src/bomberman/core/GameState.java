a811cb6a4c8b2e438daa8457451cc5dca3d9e814
Ruby040207 <24022419@vnu.edu.vn>
2025-05-10 20:31:06 +0700

    Merge GameState.java

--- bomberman/src/main/java/src/bomberman/core/GameState.java
+++ bomberman/src/main/java/src/bomberman/core/GameState.java
@@ -1,7 +1,7 @@
 package src.bomberman.core;
 
 public enum GameState {
-    PLAYING,             // Đang chơi level
+    PLAYING,            
     LEVEL_TRANSITIONING, // Đang hiển thị màn hình chuyển level
     GAME_OVER,           // Đã thua
     GAME_WON             // (Tùy chọn) Đã thắng tất cả level
